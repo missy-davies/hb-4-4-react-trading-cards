ef1c14d9edd0de2e69ad60fb885dd8b656b4494c
Missy Davies <ms.melissadavies@gmail.com>
2021-03-19 00:57:19 +0000

    Add react rendering for new card data
    Co-authored-by: Madiha Latif <madihagoheer@outlook.com>

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -2,9 +2,11 @@
 
 function Homepage() {
   return (
-    <div>
-      This component needs to be implemented.
-    </div>
+    <React.Fragment>
+      <p class='greeting'>Hello! Welcome to the Site!</p>
+      <a href="/cards" class= 'next-link'>Click here</a><br></br>
+      <img src='/static/img/balloonicorn.jpg' />
+    </React.Fragment>
   );
 }
 
--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -4,15 +4,59 @@ const tradingCardData = [
     name: 'Balloonicorn',
     skill: 'video games',
     imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpg'
+  },
+
+  {
+    name: 'Seed.py',
+    skill: 'making curry dishes',
+    imgUrl: '/static/img/seedpy.jpg'
+  },
+
+  {
+    name: 'Polymorphism',
+    skill: 'costumes',
+    imgUrl: '/static/img/polymorphism.jpg'
+  },
+
+  {
+    name: 'Short Stack Overflow',
+    skill: 'ocean animal trivia',
+    imgUrl: '/static/img/shortstack-overflow.jpg'
+  },
+
+  {
+    name: 'Merge',
+    skill: 'bullet journaling',
+    imgUrl: '/static/img/merge.jpg'
   }
-]
+];
+
 
 function TradingCard(props) {
   return (
     <div className="card">
       <h2>Name: {props.name}</h2>
       <img src={props.imgUrl} />
-      <h2>Skill: </h2>
+      <h2>Skill = {props.skill} </h2>
     </div>
   );
 }
@@ -48,4 +92,27 @@ ReactDOM.render(
     />
   ),
   document.querySelector('#llambda')
+);
+
+ReactDOM.render(
+  (
+    <TradingCard
+      name="Merge"
+      skill="coding"
+      imgUrl="/static/img/merge.jpg"
+    />
+  ),
+  document.querySelector('#merge')
+);
+
+ReactDOM.render(
+  (
+    <TradingCard
+    name="Myreen"
+    skill="playing"
+    imgUrl="/static/img/off-by-one.jpg"
+    />
+  ),
+  document.querySelector('#myreen')
+
 );
